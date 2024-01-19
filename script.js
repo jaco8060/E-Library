6ab6faeea04500bdc134936a2492b434d78be193
Jeremiah <jeremiahjacobson10@gmail.com>
2024-01-19 17:41:45 -0500

    added styles for library container

--- script.js
+++ script.js
@@ -50,21 +50,42 @@ function addBookCard(book) {
   //create elements to put in card
   const book_container = document.createElement("div");
   const title_heading = document.createElement("h1");
+  const title_value = document.createElement("h2");
   const author_heading = document.createElement("h1");
+  const author_value = document.createElement("h2");
   const pages_heading = document.createElement("h1");
-  const read_heading = document.createElement("h1");
+  const pages_value = document.createElement("h2");
+  const read_value = document.createElement("h2");
   const book_num = document.createElement("h1");
+
+  const div_group1 = document.createElement("div");
+  const div_group2 = document.createElement("div");
+  const div_group3 = document.createElement("div");
 
   const remove_img = document.createElement("img");
 
   const remove_book = document.createElement("button");
   //need to iterate through the list of objects and delete the current containers and create the new container with updated book objects each time.
 
-  title_heading.textContent = book.title;
-  author_heading.textContent = book.author;
-  pages_heading.textContent = book.pages;
-  read_heading.textContent = book.read;
+  title_heading.textContent = "Title of Book:";
+  title_value.textContent = book.title;
+  author_heading.textContent = "Author:";
+  author_value.textContent = book.author;
+  pages_heading.textContent = "# of Pages:";
+  pages_value.textContent = book.pages;
+  read_value.textContent = `Status: ${book.read}`;
   book_num.textContent = `Book ${myLibrary.indexOf(book) + 1}`;
+
+  div_group1.appendChild(title_heading);
+  div_group1.appendChild(title_value);
+  div_group2.appendChild(author_heading);
+  div_group2.appendChild(author_value);
+  div_group3.appendChild(pages_heading);
+  div_group3.appendChild(pages_value);
+  div_group1.classList.add("book-card-group");
+  div_group2.classList.add("book-card-group");
+  div_group3.classList.add("book-card-group");
+  read_value.classList.add("read-status");
 
   remove_img.src = "img/trash-can.svg";
   remove_img.alt = "Remove Book"; // Add an alt attribute for accessibility
@@ -75,12 +96,14 @@ function addBookCard(book) {
   remove_book.appendChild(remove_img);
   remove_book.classList.add("removeBook");
 
-  book_container.appendChild(title_heading);
-  book_container.appendChild(author_heading);
-  book_container.appendChild(pages_heading);
-  book_container.appendChild(read_heading);
-  book_container.appendChild(book_num);
   book_num.classList.add("book-num");
+
+  book_container.appendChild(book_num);
+  book_container.appendChild(div_group1);
+  book_container.appendChild(div_group2);
+  book_container.appendChild(div_group3);
+
+  book_container.appendChild(read_value);
   book_container.appendChild(remove_book);
   book_container.classList.add("book-container");
   const library_container = document.querySelector(".library-container");
